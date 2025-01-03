64880cf3a0c82222140664bbe4a266a06432db08
Alexandr Fedorenchik <afedorenchik@microsoft.com>
2025-01-03 15:57:16 -0500

    more tables added

---
+++ HotelApp.Data/dbo/Tables/RoomStatuses.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[RoomStatuses]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(80) NOT NULL
+)
---
+++ HotelApp.Data/dbo/Tables/Rooms.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Rooms]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Number] INT NOT NULL, 
+    [TypeId] INT NOT NULL, 
+    [StatusId] INT NOT NULL, 
+    CONSTRAINT [FK_Rooms_To_RoomStatuses] FOREIGN KEY ([StatusId]) REFERENCES [RoomStatuses]([Id]),
+    CONSTRAINT [FK_Rooms_To_RoomTypes] FOREIGN KEY ([TypeId]) REFERENCES [RoomTypes]([Id]) 
+)
