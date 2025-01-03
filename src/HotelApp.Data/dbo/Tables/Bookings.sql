18df68daecdc99d8df1c504392b70fcb17d57f6f
Alexandr Fedorenchik <afedorenchik@microsoft.com>
2025-01-03 16:09:49 -0500

    add defaults where possible

--- src/HotelApp.Data/dbo/Tables/Bookings.sql
+++ src/HotelApp.Data/dbo/Tables/Bookings.sql
@@ -5,7 +5,7 @@
     [EndDate] DATETIME NOT NULL, 
     [RoomId] INT NOT NULL, 
     [GuestId] INT NOT NULL, 
-    [StatusId] INT NOT NULL, 
+    [StatusId] INT NOT NULL DEFAULT 1, 
     [TotalCost] MONEY NOT NULL,
     CONSTRAINT [FK_Bookings_To_BookingStatuses] FOREIGN KEY ([StatusId]) REFERENCES [BookingStatuses]([Id]),
     CONSTRAINT [FK_Bookings_To_Guests] FOREIGN KEY ([GuestId]) REFERENCES [Guests]([Id]),
--- src/HotelApp.Data/dbo/Tables/Rooms.sql
+++ src/HotelApp.Data/dbo/Tables/Rooms.sql
@@ -2,8 +2,8 @@
 (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Number] NVARCHAR(10) NOT NULL, 
-    [TypeId] INT NOT NULL, 
-    [StatusId] INT NOT NULL, 
+    [TypeId] INT NOT NULL DEFAULT 1, 
+    [StatusId] INT NOT NULL DEFAULT 1, 
     CONSTRAINT [FK_Rooms_To_RoomStatuses] FOREIGN KEY ([StatusId]) REFERENCES [RoomStatuses]([Id]),
     CONSTRAINT [FK_Rooms_To_RoomTypes] FOREIGN KEY ([TypeId]) REFERENCES [RoomTypes]([Id]) 
 )
