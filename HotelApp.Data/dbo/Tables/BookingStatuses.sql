3b9bf00f31af6feaee340cd7ef9838718590369c
host1812 <host1812@gmail.com>
2025-01-08 10:53:46 -0500

    complete data layer

---
+++ HotelApp.Data/dbo/Tables/BookingStatuses.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[BookingStatuses]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(100) NOT NULL
+)
---
+++ HotelApp.Data/dbo/Tables/Bookings.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Bookings]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [RoomId] INT NOT NULL, 
+    [GuestId] INT NOT NULL, 
+    [StatusId] INT NOT NULL, 
+    [TotalCost] MONEY NOT NULL,
+    CONSTRAINT [FK_Bookings_To_BookingStatuses] FOREIGN KEY ([StatusId]) REFERENCES [BookingStatuses]([Id]),
+    CONSTRAINT [FK_Bookings_To_Guests] FOREIGN KEY ([GuestId]) REFERENCES [Guests]([Id]),
+    CONSTRAINT [FK_Bookings_To_Rooms] FOREIGN KEY ([RoomId]) REFERENCES [Rooms]([Id])
+)
