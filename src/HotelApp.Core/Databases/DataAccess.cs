e8037b8e0e06c8e10b61574c178f642bab31d851
host1812 <host1812@gmail.com>
2025-01-08 10:53:48 -0500

    prepare for data access

---
+++ src/HotelApp.Core/Databases/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Core.Databases;
+
+public class DataAccess
+{
+    private readonly IConfiguration config;
+    public DataAccess(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public List<T> LoadData<T, U>(string sql, U parameters, Action<DataAccessOptions> options, string connStringName)
+    {
+        var dataAccessOptions = new DataAccessOptions();
+        options.Invoke(dataAccessOptions);
+
+        string? connectionString = config.GetConnectionString(connStringName);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ApplicationException("Invalid configuration provided");
+        }
+        
+        using IDbConnection connection = new SqlConnection(connectionString);
+        var commandType = CommandType.Text;
+        if (dataAccessOptions.IsStoredProcesure == true)
+        {
+            commandType = CommandType.StoredProcedure;
+        }
+
+        List<T> rows = connection.Query<T>(sql, parameters, commandType: commandType).ToList();
+        return rows;
+    }
+
+    public List<T> LoadData<T, U>(string sql, U parameters, Action<DataAccessOptions> options)
+    {
+        return LoadData<T, U>(sql, parameters, options, "Default");
+    }
+
+    public List<T> LoadData<T, U>(string sql, U parameters)
+    {
+        return LoadData<T, U>(sql, parameters, x => { x.IsStoredProcesure = false; }, "Default");
+    }
+
+    public void SaveData<T>(string sql, T parameters, string connectionString, dynamic options)
+    {
+
+    }
+}
---
+++ src/HotelApp.Core/Databases/DataAccessOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Core.Databases;
+
+public class DataAccessOptions
+{
+    public bool IsStoredProcesure { get; set; }
+
+    public delegate DataAccessOptions GetOptions();
+}
