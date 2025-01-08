6194cb12413dce732ed55b80493a909100385f7b
host1812 <host1812@gmail.com>
2025-01-08 10:53:46 -0500

    add RoomTypes

---
+++ HotelApp.Data/dbo/Tables/RoomTypes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RoomTypes]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Title] NVARCHAR(250) NOT NULL,
+    [Description] NVARCHAR(2048) NOT NULL,
+    [BasePrice] MONEY NOT NULL,
+)
