c5aed883cdeb8398e0ee576aeaa8b6e64e447437
host1812 <host1812@gmail.com>
2025-01-09 09:52:15 -0500

    boostrap new guest service

--- src/HotelApp.Core/Services/GuestService.cs
+++ src/HotelApp.Core/Services/GuestService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelApp.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,29 @@ using System.Threading.Tasks;
 namespace HotelApp.Core.Services;
 public class GuestService
 {
+    public List<Guest> GetGuest()
+    {
+        return [];
+    }
+
+    public Guest GetGuestById(int id)
+    {
+        return new Guest { };
+    }
+
+    public Guest GetGuestByFirstName(string firstName)
+    {
+        return new Guest { };
+    }
+
+    public Guest GetGuestByLastName(string lastName)
+    {
+        return new Guest { };
+    }
+
+    public Guest GetGuestByEmail(string email)
+    {
+        return new Guest { };
+    }
 
 }
